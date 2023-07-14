8a5c8670821c571d904f8d80396de5693947b2d2
LiveIsAbsurd <130514590+LiveIsAbsurd@users.noreply.github.com>
2023-07-14 14:34:46 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,8 +8,8 @@ const fs = require('fs');
 const https = require('https');
 
 const options = {
-  key: fs.readFileSync('./private.key'),
-  cert: fs.readFileSync('./certificate.crt')
+  key: fs.readFileSync('./server.key'),
+  cert: fs.readFileSync('./server.cert')
 };
 
 const bot = new TelegramBot(token, {polling: { interval: 3000 }});
