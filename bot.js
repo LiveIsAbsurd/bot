ff2063b52eca556d848167d688847421773ea66c
LiveIsAbsurd <anndreybykov19@gmail.com>
2023-07-18 23:15:58 +0300

    хм

--- bot.js
+++ bot.js
@@ -8,14 +8,29 @@ const fs = require("fs");
 const https = require("https");
 const { json } = require("body-parser");
 const MongoClient = require("mongodb").MongoClient;
-const mongoUrl = "mongodb://127.0.0.1:27017/";
-const db = new MongoClient(mongoUrl);
-
-db.connect().then(dd => {
-  console.log('Всё отлично');
-  console.log(dd.options.dbName);
-  db.close().then(data => console.log("Закрыто"))
-})
+    
+const url = "mongodb://127.0.0.1:27017/";
+const mongoClient = new MongoClient(url);
+async function run() {
+    try {
+        // Подключаемся к серверу
+        await mongoClient.connect();
+        // обращаемся к базе данных admin
+        const db = mongoClient.db("admin");
+        // выполняем пинг для проверки подключения
+        const result = await db.command({ ping: 1 });
+        console.log("Подключение с сервером успешно установлено");
+        console.log(result);
+    }catch(err) {
+        console.log("Возникла ошибка");
+        console.log(err);
+    } finally {
+        // Закрываем подключение при завершении работы или при ошибке
+        await mongoClient.close();
+        console.log("Подключение закрыто");
+    }
+}
+run().catch(console.error);
 
 function hiText(username) {
   let text = `
