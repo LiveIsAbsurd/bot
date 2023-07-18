3a40d487a900992325e7951513b5b6ae93d6fc43
LiveIsAbsurd <anndreybykov19@gmail.com>
2023-07-19 00:30:31 +0300

    аааа

--- bot.js
+++ bot.js
@@ -11,45 +11,57 @@ const MongoClient = require("mongodb").MongoClient;
     
 const url = "mongodb://127.0.0.1:27017/";
 const mongoClient = new MongoClient(url);
-
-mongoClient.connect((err, client) => {
-  console.log('Connected to MongoDB');
-  const db = client.db("botDB");
-  const collection = db.collection("hiCount");
-
-  // Обработка нажатий кнопок
-  bot.on('callback_query', (query) => {
-    const { message, data } = query;
-  
-      // Получить данные из коллекции
-      collection.findOne({ buttonId: data }, (error, result) => {
-        if (error) {
-          console.error('MongoDB error:', error);
-          return;
-        }
-    
-        if (result) {
-          // Обновить текст кнопки
-          const keys = {
-            inline_keyboard: [[{text: `${result.count}`, callback_data: 'key'}]]
-          }
+async function run() {
+    try {
+        // Подключаемся к серверу
+        await mongoClient.connect();
+        // обращаемся к базе данных admin
+        const db = mongoClient.db("botDB");
+        const collection = db.collection("hiCount");
         
-    
-          bot.editMessageReplyMarkup(keys, { chat_id: query.message.chat.id, message_id: messageId });
-    
-          // Увеличить счетчик нажатий кнопки в базе данных
-          collection.updateOne({ buttonId: data }, { $inc: { count: 1 } }, { upsert: true }, (error, result) => {
-            if (error) {
-              console.error('MongoDB error:', error);
-              return;
-            }
-            console.log('Button click counter incremented');
-          });
-        }
-      });
-
-  });
-}).catch(err => console.log(err))
+        bot.on('callback_query', (query) => {
+          const { message, data } = query;
+        
+            // Получить данные из коллекции
+            collection.findOne({ buttonId: data }, (error, result) => {
+              if (error) {
+                console.error('MongoDB error:', error);
+                return;
+              }
+          
+              if (result) {
+                // Обновить текст кнопки
+                const keys = {
+                  inline_keyboard: [[{text: `${result.count}`, callback_data: 'key'}]]
+                }
+              
+          
+                bot.editMessageReplyMarkup(keys, { chat_id: query.message.chat.id, message_id: messageId });
+          
+                // Увеличить счетчик нажатий кнопки в базе данных
+                collection.updateOne({ buttonId: data }, { $inc: { count: 1 } }, { upsert: true }, (error, result) => {
+                  if (error) {
+                    console.error('MongoDB error:', error);
+                    return;
+                  }
+                  console.log('Button click counter incremented');
+                });
+              }
+            });
+      
+        });
+        console.log("Подключение с сервером успешно установлено");
+        console.log(result);
+    }catch(err) {
+        console.log("Возникла ошибка");
+        console.log(err);
+    } finally {
+        // Закрываем подключение при завершении работы или при ошибке
+        await mongoClient.close();
+        console.log("Подключение закрыто");
+    }
+}
+run().catch(console.error);
 
 function hiText(username) {
   let text = `
@@ -78,6 +90,7 @@ https
   });
 
 const bot = new TelegramBot(token, { polling: true });
+
 
 bot.onText(/\/getKey/, (msg) => {
 
