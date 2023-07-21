02d849291236817b16854dd73d201ff6af3f006c
LiveIsAbsurd <anndreybykov19@gmail.com>
2023-07-21 22:37:46 +0300

    f

--- bot.js
+++ bot.js
@@ -20,7 +20,9 @@ function hiText(username) {
 
 let test = {test: 5};
 
-fs.writeFile("../cuefaStats.json", JSON.stringify(test), "UTF-8");
+fs.writeFile("../cuefaStats.json", JSON.stringify(test), "UTF-8", (err) => {
+  console.log(err)
+});
 
 const bot = new TelegramBot(token, { polling: { interval: 1000 } });
 
