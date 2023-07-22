b9268c4a01be43f610f6379f3a14766f48d5459b
LiveIsAbsurd <anndreybykov19@gmail.com>
2023-07-22 14:31:03 +0300

    очистка ID

--- bot.js
+++ bot.js
@@ -395,6 +395,8 @@ ${winner}
           ).then(() => {
             delete cuefaColl[query.message.message_id];
             delete cuefaPlayers[query.message.message_id];
+            delete cuefaPlayer1Id[query.message.message_id];
+            delete cuefaPlayer2Id[query.message.message_id];
             })
         });
       });
