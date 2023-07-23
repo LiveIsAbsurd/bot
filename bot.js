9e477b4d0f4bbd924856fea2e884a67e109bf9a4
LiveIsAbsurd <anndreybykov19@gmail.com>
2023-07-23 23:28:38 +0300

    таймаут очистки сессий

--- bot.js
+++ bot.js
@@ -405,11 +405,13 @@ ${winner}
               }
             }
           ).then(() => {
-            delete cuefaColl[query.message.message_id];
-            delete cuefaPlayers[query.message.message_id];
-            delete cuefaPlayer1Id[query.message.message_id];
-            delete cuefaPlayer2Id[query.message.message_id];
-            })
+            setTimeout(() => {
+              delete cuefaColl[query.message.message_id];
+              delete cuefaPlayers[query.message.message_id];
+              delete cuefaPlayer1Id[query.message.message_id];
+              delete cuefaPlayer2Id[query.message.message_id];
+            }, 5000);
+          })
         });
       });
     }
