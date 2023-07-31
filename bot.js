1067897b8e0f9ae1e4cf930278a547e21ffee2f4
LiveIsAbsurd <anndreybykov19@gmail.com>
2023-07-31 15:04:09 +0300

    ff

--- bot.js
+++ bot.js
@@ -29,11 +29,6 @@ const bot = new TelegramBot(token, { polling: { interval: 1000 } });
 let chatState = JSON.parse(fs.readFileSync("../chatStats.json", "UTF-8"),null, 2);
 let editState = false;
 
-bot.editMessageText(`Сообщений с 27.07.2023: ${chatState.totalMessage}`, {
-  chat_id: "-1001807749316",
-  message_id: "59131"
-});
-
 bot.on("message", (msg) => {
   if (msg.chat.id == "-1001807749316") {
     setChatState(msg, chatState);
