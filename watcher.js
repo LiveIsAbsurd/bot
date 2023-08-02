04ae09b0ce982840bd9ca73132a2020b4a3cb2f9
LiveIsAbsurd <anndreybykov19@gmail.com>
2023-08-02 14:05:40 +0300

    новый бот

---
+++ watcher.js
@@ -0,0 +1,35 @@
+const token = require("../watcher.js");
+const TelegramBot = require("node-telegram-bot-api");
+const fs = require("fs");
+const bot = new TelegramBot(token, { polling: { interval: 1000 } });
+
+let triggers = JSON.parse(fs.readFileSync("./file.json", "UTF-8"), null, 2);
+
+bot.on("message", msg => {
+  let text = msg.text.toLowerCase();
+  let trigArr = Object.keys(triggers);
+
+  trigArr.forEach(el => {
+    if (text.includes(el)) {
+      let rnd = Math.floor(Math.random() * (triggers[el].length));
+      bot.sendMessage(msg.chat.id, triggers[el][rnd], {reply_to_message_id: msg.message_id});
+    }
+  })
+})
+
+bot.onText(/\/addTrigger (.+)/, (msg, match) => {
+  if (msg.from.id == "261749882" || msg.from.id == "300711096") {
+    let trigger = match[1];
+    let trigText = match.input.replace(match[0], "").replace("\n", "")
+
+    if (!triggers[trigger]) {
+      triggers[trigger] = [];
+    }
+
+    triggers[trigger].push(trigText);
+
+    fs.writeFile("./file.json", JSON.stringify(triggers, null, 2), "utf-8", err => {
+      bot.sendMessage(msg.chat.id, `Триггер: ${trigger} \nтекст: ${trigText}`);
+    });
+  }
+})
