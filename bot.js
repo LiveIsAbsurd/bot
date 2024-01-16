29133373b3c75d6df99f6f19c16faa129ea94a9c
LiveIsAbsurd <anndreybykov19@gmail.com>
2024-01-16 19:20:20 +0300

    svs

--- bot.js
+++ bot.js
@@ -1242,9 +1242,9 @@ const yestUsers = () => {
   const yestDate = `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate() - 1}`;
   const usersState = { ...chatState.messageOnDate[yestDate].userMessage }
   const users = Object.keys(usersState).map(key => [key, usersState[key]]);
-  // const sortUsers = users.sort((a, b) => {
-  //   return b.count - a.count;
-  // });
+  const sortUsers = users.sort((a, b) => {
+    return b[1].count - a[1].count;
+  });
 
-  console.log(users);
+  console.log(sortUsers);
 };
