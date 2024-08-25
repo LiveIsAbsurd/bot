9edddfd5d31394f7f00648a546f880b5faa1c45d
LiveIsAbsurd <anndreybykov19@gmail.com>
2024-08-26 00:36:56 +0300

    фикс

--- bot.js
+++ bot.js
@@ -95,8 +95,7 @@ const getChatAdmins = () => {
       .then((response) => {
         response.data.result.forEach((admin) => {
           if (admin.can_restrict_members || admin.status == "creator") {
-            const username = admin.user.username;
-            adminList.push(username.toLowerCase());
+            adminList.push(admin.user.id);
           }
         });
         console.log(adminList);
@@ -463,7 +462,7 @@ bot.onText(/\/kick/, (msg) => {
   const userId = msg.reply_to_message.from.id;
 
   if (chatId == "-1001807749316") {
-      if (adminList.includes(msg.from.username.toLowerCase)) {
+      if (adminList.includes(msg.from.id)) {
         axios
           .get(
             `https://api.telegram.org/bot${token}/kickChatMember?chat_id=${chatId}&user_id=${userId}`
@@ -1144,7 +1143,7 @@ const muteUser = (msg) => {
   if (msg.chat.id != "-1001807749316") {
     return;
   };
-  if (adminList.includes(msg.from.username.toLowerCase)) {
+  if (adminList.includes(msg.from.id)) {
     const user = msg.reply_to_message.from.id;
     const time = msg.text.replace('/mute', '').trim() ? msg.text.replace('/mute', '').trim() : 3600;
     const untilDate = Math.floor(Date.now() / 1000) + Number(time);
