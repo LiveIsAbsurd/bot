87ae453798e9916796c7c438783e435ddddf493e
LiveIsAbsurd <anndreybykov19@gmail.com>
2025-05-02 12:43:26 +0300

    f

--- api.js
+++ api.js
@@ -118,7 +118,6 @@ app.get("/iswork", (req, res) => {
 app.get("/searchVendor/:mac", (req, res) => {
     axios.get(`https://api.maclookup.app/v2/macs/${req.params.mac}`)
         .then(response => {
-            console.log(response.data);
             res.header("Access-Control-Allow-Origin", "*");
             res.header("Access-Control-Allow-Methods", "GET");
             res.json(response.data.company == "" ? "Не известно" : response.data.company);
